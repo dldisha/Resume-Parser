b02fd17be5197fac369b1e839cef78454e8493a8
dldisha <dldisha929@gmail.com>
2019-12-05 21:33:56 +0530

    initialization

---
+++ main.py
@@ -0,0 +1,151 @@
+import io
+
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfpage import PDFPage
+import spacy
+from spacy.matcher import Matcher
+from nltk.corpus import stopwords
+import pandas as pd
+import re
+
+# load pre-trained model
+nlp = spacy.load("en_core_web_sm")
+#noun_chunks = nlp.noun_chunks
+
+# initialize matcher with a vocab
+matcher = Matcher(nlp.vocab)
+
+#Segmentation
+def extract_text_from_pdf(pdf_path):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle)
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    with open(pdf_path, 'rb') as fh:
+        for page in PDFPage.get_pages(fh,
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+        text = fake_file_handle.getvalue()
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+
+    if text:
+        return text
+
+#Name
+def extract_name(resume_text):
+    nlp_text = nlp(resume_text)
+
+    for chunk in nlp_text.noun_chunks:
+        return chunk
+
+#Phone Number
+def extract_mobile_number(temp):
+    phone = re.findall(re.compile(
+        r'(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?'),
+                       temp)
+
+    if phone:
+        number = ''.join(phone[0])
+        if len(number) > 10:
+            return '+' + number
+        else:
+            return number
+
+#Email
+def extract_email(email):
+    email = re.findall("([^@|\s]+@[^@]+\.[^@|\s]+)", email)
+    if email:
+        try:
+            return email[0].split()[0].strip(';')
+        except IndexError:
+            return None
+
+#Skills
+def extract_skills(resume_text):
+    nlp_text = nlp(resume_text)
+
+    # removing stop words and implementing word tokenization
+    tokens = [token.text for token in nlp_text if not token.is_stop]
+
+    # reading the csv file
+    data = pd.read_csv("skill.csv")
+
+    # extract values
+    skills = list(data.columns.values)
+
+    skillset = []
+
+    # check for one-grams (example: python)
+    for token in tokens:
+        if token.lower() in skills:
+            skillset.append(token)
+
+    # check for bi-grams and tri-grams (example: machine learning)
+    for token in nlp_text.noun_chunks:
+        token = token.text.lower().strip()
+        if token in skills:
+            skillset.append(token)
+
+    return [i.capitalize() for i in set([i.lower() for i in skillset])]
+
+#Education
+
+# Grad all general stop words
+STOPWORDS = set(stopwords.words('english'))
+
+# Education Degrees
+EDUCATION = [
+            'Bachelor of Computer Science','BE','B.E.', 'B.E', 'BS', 'B.S',
+            'ME', 'M.E', 'M.E.', 'MS', 'M.S',
+            'BTECH', 'B.TECH', 'M.TECH', 'MTECH',
+            'SSC', 'HSC', 'CBSE', 'ICSE', 'X', 'XII'
+        ]
+
+def extract_education(resume_text):
+    nlp_text = nlp(resume_text)
+
+    # Sentence Tokenizer
+    nlp_text = [sent.string.strip() for sent in nlp_text.sents]
+
+    edu = {}
+    # Extract education degree
+    for index, text in enumerate(nlp_text):
+        for tex in text.split():
+            # Replace all special symbols
+            tex = re.sub(r'[?|$|.|!|,]', r'', tex)
+            if tex.upper() in EDUCATION and tex not in STOPWORDS:
+                edu[tex] = text + nlp_text[index + 1]
+
+    # Extract year
+    education = []
+    for key in edu.keys():
+        year = re.search(re.compile(r'(((20|19)(\d{2})))'), edu[key])
+        if year:
+            education.append((key, ''.join(year[0])))
+        else:
+            education.append(key)
+    return education
+
+
+if __name__ == '__main__':
+    temp = extract_text_from_pdf('resume_location')
+    text = [line.replace('\n', ' ') for line in temp.split('\n') if line]
+    print(temp, '\n')
+    name = extract_name(temp)
+    print('Name:', name, '\n')
+    num = extract_mobile_number(temp)
+    print('Phone Number:', num, '\n')
+    mail = extract_email(temp)
+    print('Email:', mail, '\n')
+    skill = extract_skills(temp)
+    print('Skills are:', skill, '\n')
+    edu = extract_education(temp)
+    print('Education:', edu, '\n')
+
+
+
